2674ab3159510910bdc1786651f2c30249b7c5df
nikita <roikn33@mail.ru>
2024-05-30 20:49:41 +0300

    task2

--- hw.py
+++ hw.py
@@ -7,6 +7,20 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecturer(self, lector, lec, grade):
+        if isinstance(lector, Lecturer) and lec in lector.lec:
+            if lec in lector.grades:
+                lector.grades[lec] += [grade]
+            else:
+                lector.grades[lec] = [grade]
+        else:
+            return 'Ошибка'
+
+student_list = []
+student_list.append(Student('Mihail', 'Evdokimov', 'male'))
+student_list.append(Student('Aleksandr', 'Schvetsov', 'male'))
+student_list.append(Student('Anastasia', 'Tagulova', 'female'))
+student_list.append(Student('Polina', 'Karmanova', 'female'))
 
 class Mentor:
     def __init__(self, name, surname):
@@ -14,6 +28,22 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super.__init__(name, surname)
+        self.name = name
+        self.surname = surname
+        self.lec = []
+        self.grades = {}
+
+lecturers_list = []
+lecturers_list.append(Lecturer('Steve', 'Jobs'))
+lecturers_list.append(Lecturer('Faraon', 'Tutanhamon'))
+
+lecturers_list[0].lec += ['Python', 'Git']
+lecturers_list[1].lec += ['Python', 'Git']
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -23,8 +53,5 @@ class Mentor:
         else:
             return 'Ошибка'
 
-class Lecturer(Mentor):
-    pass
-
-class Reviewer(Mentor):
-    pass
+mentors_list.append(Reviewer('Konstantin', 'Vorobiev'))
+mentors_list.append(Reviewer('Georgiy', 'Chivchyan'))
